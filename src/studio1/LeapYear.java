eb889b79193ab049ce5807d14c49e57c8c2e6d2a
ashergarren <ashergarren@localhost>
2024-09-04 14:16:59 -0500

    Done with studio 1!

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,13 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the year?");
+	}
+
+}
--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,13 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean isOrdered = true;
+		if((x<y) && (y<z) || (x>y) && (y>z)){
+			System.out.println(isOrdered);
+		}
+		else {
+			System.out.println("False");
+		}
 	}
 
 }
